a7b443f4622881d6b60d9820a8a878ee44f40717
KFlorida24 <karlkoyoflorida@gmail.com>
2023-11-29 04:19:16 -0500

    Update homoTOXMaterialModuleInput.py

--- TOX_Project/Fall_Sem_Final/Finite (working folder)/Spyder Project/homoTOXMaterialModuleInput.py
+++ TOX_Project/Fall_Sem_Final/Finite (working folder)/Spyder Project/homoTOXMaterialModuleInput.py
@@ -303,20 +303,15 @@ def matMixFunInput():
         source.angle = openmc.stats.Isotropic()
         #source.energy = openmc.stats.Discrete([10.0e6], [1.0])
         #source.time = openmc.stats.Uniform(0, 1e-6)
+        
         settings = openmc.Settings()
         settings.source = source
-        settings.run_modes = 'fixed source'
-    
-        settings = openmc.Settings()
-        settings.source = source
+        #settings.run_modes = 'fixed source'
+        settings.run_mode = 'eigenvalue';
         settings.temperature['method']='interpolation';
         settings.batches = n_batches
         settings.inactive = n_inactive
         settings.particles = n_particlesPerBatch
-        
-        # settings.particles = 10000 # particles per batch
-        # settings.batches = 1050 # number of batches
-        # settings.inactive = 50 # inactive batches
     
         
         cell_filter = openmc.CellFilter(material_Geom)
@@ -368,8 +363,8 @@ def matMixFunInput():
         # openmc.plot command documentation: https://docs.openmc.org/en/stable/pythonapi/generated/openmc.Plot.html
         # openmc.plot command color documentation: https://www.w3.org/TR/SVG11/types.html#ColorKeywords
         Image('homogeneousMatMixPlotXZ.png')
-        plotsXZ.to_ipython_image()
-        plotsXZ.origin = (0,0,cyl_Length/2)
+        plotXZ.to_ipython_image()
+        plotXZ.origin = (0,0,cyl_Length/2)
 
 
        
@@ -664,21 +659,16 @@ def matMixFunInput():
                 source.angle = openmc.stats.Isotropic()
                 #source.energy = openmc.stats.Discrete([10.0e6], [1.0])
                 #source.time = openmc.stats.Uniform(0, 1e-6)
-                settings = openmc.Settings()
-                settings.source = source
-                settings.run_modes = 'fixed source'
             
                 settings = openmc.Settings()
                 settings.source = source
-                settings.temperature['method']='interpolation';
+                #settings.run_modes = 'fixed source'
+                settings.run_mode = 'eigenvalue'
+                settings.temperature['method']='interpolation'
                 settings.batches = n_batches
                 settings.inactive = n_inactive
                 settings.particles = n_particlesPerBatch
                 
-                # settings.particles = 10000 # particles per batch
-                # settings.batches = 1050 # number of batches
-                # settings.inactive = 50 # inactive batches
-            
                 
                 cell_filter = openmc.CellFilter(material_Geom)
                 
@@ -773,69 +763,287 @@ def matMixFunInput():
                 break
         while True:
             try:
-                enrichRunNum = int(input('Desired number of enrichments tested? (Press Enter key for default value of 2)') or 2)
+                UO2Enrichment = float(input('Desired level of UO2 Enrichment (%)? (Press Enter key for default for default value of 15%)') or 15)/100
             except ValueError:
-                print("Please type in a valid number (must be an integer, no characters)")
-                continue
-            if enrichRunNum < 2:
-                print("# of runs must be more than 1")
+                print("Please type in a valid number (no characters)")
                 continue
             else:
                 break
         while True:
             try:
-                fuelMixRunNum = int(input('Desired number of fuel mixtures tested? (Press Enter key for default value of 2)') or 2)
+                pctLEU = float(input('Desired TOX Fuel Uranium Fraction (%)? (Press Enter key for default for default value of 40%)') or 40)/100
             except ValueError:
                 print("Please type in a valid number (no characters)")
                 continue
-            if fuelMixRunNum < 2:
-                print("# of runs must be more than 1")
+            else:
+                break
+        print('UO2 Mass Density: ', UO2MassRho, 'g/cc')
+        print('UO2 Enrichment: ', UO2Enrichment*100, '%')
+        print('Fuel UO2 Percent: ', pctLEU*100, '%')
+        print('Fuel Thorium Percent: ', (1 - pctLEU)*100, '%')
+        while True:
+            try:
+                n_batches = int(input('How many batches per run? (Press Enter key for default for default value of 110)') or 110)
+            except ValueError:
+                print("Please type in a valid integer (no characters)")
+                continue
+            if n_batches <= 0:
+                print("Must be greater than 0")
                 continue
             else:
                 break
         while True:
             try:
-                UO2EnrichmentFirst = float(input('Starting level of UO2 Enrichment (%)? (Press Enter key for default value of 15%)') or 15)/100
+                n_inactive = int(input('How many discarded batches per run? (Press Enter key for default for default value of 10)') or 10)
             except ValueError:
-                print("Please type in a valid number (no characters)")
+                print("Please type in a valid integer (no characters)")
                 continue
-            if UO2EnrichmentFirst < 0:
-                print("Value must be greater than 0.")
+            if n_batches <= n_inactive:
+                print("Must be less than the number of batches run")
                 continue
             else:
                 break
         while True:
             try:
-                UO2EnrichmentLast = float(input('Final level of UO2 Enrichment (%)? (Press Enter key for default value of 19.75%)') or 19.75)/100
+                n_particlesPerBatch = int(input('How many particles ber batch? (Press Enter key for default for default value of 10000)') or 10000)
             except ValueError:
-                print("Please type in a valid number (no characters)")
+                print("Please type in a valid integer (no characters)")
                 continue
-            if UO2EnrichmentLast <= UO2EnrichmentFirst:
-                print("Final value must be greater than starting value.")
+            if n_particlesPerBatch <= 0:
+                print("Must be greater than 0")
                 continue
             else:
                 break
-        while True:
-            try:
-                pctLEUFirst = float(input('Starting TOX Fuel Uranium Fraction (%)? (Press Enter key for default for default value of 40%)') or 40)/100
-            except ValueError:
-                print("Please type in a valid number (no characters)")
-                continue
-            if pctLEUFirst < 0:
-                print("Value must be greater than 0.")
-                continue
-            else:
-                break
-        while True:
-            try:
-                pctLEULast = float(input('Final TOX Fuel Uranium Fraction (%)? (Press Enter key for default for default value of 95%)') or 95)/100
-            except ValueError:
-                print("Please type in a valid number (no characters)")
-                continue
-            if pctLEULast <= pctLEUFirst:
-                print("Final value must be greater than starting value.")
-                continue
-            else:
-                break
+        print('# of batches: ', n_batches)
+        print('# of discarded batches: ', n_inactive)
+        print('# of particles per batch: ', n_particlesPerBatch)
         
+        ## Calculate Weight Percentages ##
+        ThO2MolW = ThMolW + 2*OMolW # Molecular Weight of ThO2 [g/mol]
+        
+        # Results of Constants and Input Parameters
+        # Weight percent of Th in LEU mixture
+        pctTh = 1 - pctLEU 
+        # Total Mixture Density
+        UThMixMassRho = (pctLEU/UO2MassRho + pctTh/ThO2MassRho)**-1 # [g/cm]
+        # UO2 Atom Densities
+        UO2UMolW = ((UO2Enrichment/U235MolW) + ((1 - UO2Enrichment)/U238MolW))**-1 # Molecular Weight of U in UO2 [g/mol]
+        UO2MolW = UO2UMolW + 2*16 # Molecular Weight of UO2 [g/mol]
+        UO2OAtomRho = (UO2MassRho/UO2MolW)*2*avo # O Atom Density in UO2 [atom/b-cm]
+        UO2U235AtomRho = (UO2Enrichment*UO2MassRho*(UO2UMolW/UO2MolW)/U235MolW)*avo # U-235 Atom Density in UO2 [atom/b-cm] 
+        UO2U238AtomRho = ((1 - UO2Enrichment)*UO2MassRho*(UO2UMolW/UO2MolW)/U238MolW)*avo # U-238 Atom Density in UO2 [atom/b-cm]
+        UO2U235AtomRhoFrac = UO2U235AtomRho/(UO2U235AtomRho+UO2U238AtomRho)
+        UO2U238AtomRhoFrac = UO2U238AtomRho/(UO2U235AtomRho+UO2U238AtomRho)
+        # UO2 ThO2 Mixture Atom Densities
+        UThMixOAtomRho = (pctTh*UThMixMassRho/ThO2MolW)*2*avo + (pctLEU*UThMixMassRho/UO2MolW)*2*avo # Oxygen Atom Density in Fuel Mixture [atom/b-cm]
+        UThMixThAtomRho = (pctTh*UThMixMassRho/ThO2MolW)*avo # Uranium Atom Density in Fuel Mixture [atom/b-cm] 
+        UThMixU235AtomRho = (pctLEU*UThMixMassRho/UO2MolW)*avo*UO2U235AtomRhoFrac # Uranium Atom Density in Fuel Mixture [atom/b-cm]
+        UThMixU238AtomRho = (pctLEU*UThMixMassRho/UO2MolW)*avo*UO2U238AtomRhoFrac # Uranium Atom Density in Fuel Mixture [atom/b-cm]
+        # UO2 ThO2 Mixture Atom Fractions
+        # Normalized to 1
+        UThMixOAtomFrac = UThMixOAtomRho/(UThMixOAtomRho + UThMixThAtomRho + UThMixU235AtomRho + UThMixU238AtomRho) # Oxygen Atom Fraction in Fuel Mixture [atom/b-cm]
+        UThMixThAtomFrac = UThMixThAtomRho/(UThMixOAtomRho + UThMixThAtomRho + UThMixU235AtomRho + UThMixU238AtomRho) # Uranium Atom Fraction in Fuel Mixture [atom/b-cm] 
+        UThMixU235AtomFrac = UThMixU235AtomRho/(UThMixOAtomRho + UThMixThAtomRho + UThMixU235AtomRho + UThMixU238AtomRho) # Uranium Atom Fraction in Fuel Mixture [atom/b-cm]
+        UThMixU238AtomFrac = UThMixU238AtomRho/(UThMixOAtomRho + UThMixThAtomRho + UThMixU235AtomRho + UThMixU238AtomRho) # Uranium Atom Density in Fuel Mixture [atom/b-cm]
+        # Normalized to 3
+        UThMixOAtomFrac3 = UThMixOAtomFrac*3 # Oxygen Atom Fraction in Fuel Mixture [atom/b-cm]
+        UThMixU235AtomFrac3 = UThMixU235AtomFrac*3 # Uranium Atom Fraction in Fuel Mixture [atom/b-cm] 
+        UThMixU238AtomFrac3 = UThMixU238AtomFrac*3 # Uranium Atom Fraction in Fuel Mixture [atom/b-cm]
+        UThMixThAtomFrac3 = UThMixThAtomFrac*3 # Uranium Atom Density in Fuel Mixture [atom/b-cm]
+        
+        # Volume Parameters
+        # Cylinder
+        cylinderVol = math.pi*cyl_Radius**2*cyl_Length #[m^3]
+        clad_cyl_Radius = 1.1*cyl_Radius
+        clad_cyl_Length = 1.05*cyl_Length
+        clad_Thickness = (clad_cyl_Length - cyl_Length)/2.
+        cladVol = math.pi*clad_cyl_Radius**2*clad_cyl_Length #[m^3]
+        clad_rho = 6.56 #[g/cm^3]
+        
+        # Volume Output
+        # Cylinder
+        fuelCylinderVol = fuelVolFrac*cylinderVol
+        graphCylinderVol = graphVolFrac*cylinderVol
+        hel_CoolCylinderVol = hel_CoolVolFrac*cylinderVol
+        totalCylinderVol = fuelCylinderVol + graphCylinderVol + hel_CoolCylinderVol
+        
+        # Mass Output
+        #Cylinder
+        fuelCylinderMass = UThMixMassRho*graphCylinderVol
+        graphCylinderMass = graphMassRho*graphCylinderVol
+        hel_CoolCylinderMass = he_CoolMassRho*graphCylinderVol
+        totalCylinderMass = fuelCylinderMass + graphCylinderMass + hel_CoolCylinderMass
+
+   
+        ## Define Materials ##
+        fuel = openmc.Material(name='fuel');
+        #The below ratios were calculated assuming 95%LEU-5%Th Oxide Fuel.
+        fuel.add_nuclide('Th232', UThMixThAtomFrac, 'ao')
+        fuel.add_nuclide('U235', UThMixU235AtomFrac, 'ao') #5% U-235 enrichment
+        fuel.add_nuclide('U238', UThMixU238AtomFrac, 'ao')
+        fuel.add_element('O', UThMixOAtomFrac, 'ao')
+        fuel.set_density('g/cm3', UThMixMassRho) # Based on assumption of fuel density within TRISO
+        fuel.volume = cylinderVol
+
+        # Establish Graphite Moderator material
+        graph = openmc.Material(name='graph')
+        # add nuclides to graph
+        graph.add_element('C', 1.00)
+        graph.set_density('g/cm3', graphMassRho)
+   
+        # Establish Helium Coolant material
+        hel_Cool = openmc.Material(name='hel_Cool')
+        # add nuclides to hel_Cool
+        hel_Cool.add_nuclide('He3', 0.000002)
+        hel_Cool.add_nuclide('He4', 0.999998)
+        hel_Cool.set_density('g/cm3', he_CoolMassRho)
+
+        #materials = openmc.Materials([fuel, graph, hel_Cool])
+        mixMat = openmc.Material.mix_materials([fuel,graph,hel_Cool],
+                                           fracs=[fuelVolFrac,graphVolFrac,hel_CoolVolFrac],
+                                          percent_type='vo',
+                                          )
+        # Define Cladding Material
+        # Zircaloy 4
+        # ref 1: https://www.azom.com/article.aspx?ArticleID=7644
+        # ref 2: https://www.sciencedirect.com/topics/engineering/zircaloy#:~:text=Zircaloy%2D4%20was%20obtained%20by,%E2%80%93%20O%20(0.003%20wt%25).
+        cladMat = openmc.Material(name = 'cladMat')
+        cladMat.add_element('Zr', 0.98197)
+        cladMat.add_element('Sn', 0.015)
+        cladMat.add_element('Fe', 0.002)
+        cladMat.add_element('Cr', 0.001)
+        cladMat.add_element('O', 0.00003)      
+        cladMat.set_density('g/cm3', clad_rho) # Based on assumption of fuel density within TRISO
+        cladMat.volume = cladVol
+        
+        
+        materials = openmc.Materials()
+        #materials += [fuel, graph, hel_Cool]
+        materials += [mixMat, cladMat]
+   
+        materials.export_to_xml()
+        
+        ## Define Universe Geometry
+        
+        universeCylinder = openmc.model.RightCircularCylinder([0, 0, -0.25*cyl_Length], 1.5*cyl_Length, 1.5*cyl_Radius, axis='z')
+    
+        insideCylinder = -universeCylinder
+        # outsideCylinder = +universeCylinder #unused
+    
+        cell = openmc.Cell()
+        cell.region = insideCylinder
+    
+        universe = openmc.Universe()
+        universe.add_cell(cell)
+        
+        ## Define Bounding Geometry ##
+        matCylinder = openmc.model.RightCircularCylinder([0, 0, 0], cyl_Length, cyl_Radius, axis='z')
+        cladCylinder = openmc.model.RightCircularCylinder([0, 0, -clad_Thickness], clad_cyl_Length, 
+                                                          clad_cyl_Radius, axis='z',
+                                                          boundary_type='vacuum') #arbitrarily decided cladding width (may have to adjust)
+        material_region = -matCylinder
+        clad_region = -cladCylinder & +matCylinder
+        
+        # fuel
+        material_Geom = openmc.Cell(name='material_Geom')
+        material_Geom.fill = mixMat
+        material_Geom.region = material_region
+        
+        # cladding
+        clad_Geom = openmc.Cell(name='clad_Geom')
+        clad_Geom.fill = cladMat
+        clad_Geom.region = clad_region
+        
+
+        root_universe = openmc.Universe(cells=[material_Geom, clad_Geom])
+
+        geometry = openmc.Geometry()
+        geometry.root_universe = root_universe
+        
+        geometry.export_to_xml()
+        
+        
+        ## Cross Sections ##
+        ## Source ##
+        # create a point source
+        # ref: https://openmc.discourse.group/t/cylindrical-geometry-more-than-95-of-external-source-sites-sampled-were-rejected/1432
+        source = openmc.Source()
+        rad_src = openmc.stats.Uniform(a=0, b=cyl_Radius)
+        phi_src = openmc.stats.Uniform(a=0, b=2*math.pi)
+        z_src = openmc.stats.Uniform(a=0, b=cyl_Length)
+        origin_src = (0.0, 0.0, cyl_Length/2)
+        source.space = openmc.stats.CylindricalIndependent(r=rad_src,phi=phi_src,z=z_src,origin = origin_src)
+        source.angle = openmc.stats.Isotropic()
+        #source.energy = openmc.stats.Discrete([10.0e6], [1.0])
+        #source.time = openmc.stats.Uniform(0, 1e-6)
+
+        settings = openmc.Settings()
+        settings.source = source
+        #settings.run_modes = 'fixed source'
+        settings.run_mode = 'eigenvalue'
+        settings.temperature['method']='interpolation'
+        settings.batches = n_batches
+        settings.inactive = n_inactive
+        settings.particles = n_particlesPerBatch
+        
+        # settings.particles = 10000 # particles per batch
+        # settings.batches = 1050 # number of batches
+        # settings.inactive = 50 # inactive batches
+    
+        
+        cell_filter = openmc.CellFilter(material_Geom)
+        
+        tally = openmc.Tally()
+        tally.filters = [cell_filter]
+    
+        tally.nuclides = ['U235']
+        tally.scores = ['total','fission','absorption','(n,gamma)']
+       
+        tallies = openmc.Tallies([tally])
+        tallies.export_to_xml()
+
+        openmc.run(output=False)
+        
+        
+        # Plot XY Geometry
+        plotXY = openmc.Plot()
+        plotXY.filename = 'homogeneousMatMixPlotXY'
+        plotXY.width = (2*clad_cyl_Radius*1.1,2*clad_cyl_Radius*1.1)
+        plotXY.pixels = (600,600)
+        plotXY.color_by = 'material'
+        plotXY.colors = {mixMat: 'yellow',cladMat: 'blue'}
+        plotXY.basis = 'xy'
+        
+        plotsXY = openmc.Plots([plotXY])
+        plotsXY.export_to_xml()
+
+        openmc.plot_geometry()
+        # openmc.plot command documentation: https://docs.openmc.org/en/stable/pythonapi/generated/openmc.Plot.html
+        # openmc.plot command color documentation: https://www.w3.org/TR/SVG11/types.html#ColorKeywords
+        Image('homogeneousMatMixPlotXY.png')
+        plotXY.to_ipython_image()
+        plotXY.origin = (0,0,cyl_Length/2)
+        
+        # Plo XZ Geometry
+        plotXZ = openmc.Plot()
+        plotXZ.filename = 'homogeneousMatMixPlotXZ'
+        plotXZ.width = (2*clad_cyl_Radius*1.1,clad_cyl_Length*1.1)
+        plotXZ.pixels = (600,600)
+        plotXZ.color_by = 'material'
+        plotXZ.colors = {mixMat: 'yellow',cladMat: 'blue'}
+        plotXZ.basis = 'xz'
+        
+        plotsXZ = openmc.Plots([plotXZ])
+        plotsXZ.export_to_xml()
+
+        openmc.plot_geometry()
+        # openmc.plot command documentation: https://docs.openmc.org/en/stable/pythonapi/generated/openmc.Plot.html
+        # openmc.plot command color documentation: https://www.w3.org/TR/SVG11/types.html#ColorKeywords
+        Image('homogeneousMatMixPlotXZ.png')
+        plotXZ.to_ipython_image()
+        plotXZ.origin = (0,0,cyl_Length/2)
+        
+        ## Burn Up Section
+        
+        fuel.depletable = True
         #While True
