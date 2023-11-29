bc780dd36e8b912499339ccba05bd72507367c4b
KFlorida24 <karlkoyoflorida@gmail.com>
2023-11-29 07:56:02 -0500

    Update homoTOXFinite.py

--- TOX_Project/Fall_Sem_Final/Finite (working folder)/Spyder Project/homoTOXFinite.py
+++ TOX_Project/Fall_Sem_Final/Finite (working folder)/Spyder Project/homoTOXFinite.py
@@ -5,10 +5,11 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 import homoTOXMaterialModuleInput as matMixMod
+import homoTOXMaterialModuleInput_Infinite as matMixMod_Infinite
 import matplotlib.pyplot as plt
 import openmc.deplete
 import xml.etree.ElementTree as et
 
 
 #matMixOut = matMixMod.matMixFunInput()
-matMixOut = matMixMod.matMixFunInput_Infinite()
+matMixOut = matMixMod_Infinite.matMixFunInput()
